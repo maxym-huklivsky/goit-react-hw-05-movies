53d924cf4b7aabb9a8e2f608d0d5bac4ba0defe5
maxym-huklivsky <106371809+maxym-huklivsky@users.noreply.github.com>
2023-02-13 15:06:21 +0000

    Deploying to gh-pages from @ maxym-huklivsky/goit-react-hw-05-movies@9b027ad9ceb86b2772269e3187b6e85dddafd87f 🚀

--- static/js/922.0466ca65.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{777:function(n,e,r){r.d(e,{j:function(){return a}});var t,c=r(168),u=r(934).Z.h2(t||(t=(0,c.Z)(["\n  color: red;\n  text-align: center;\n"]))),o=r(184),a=function(n){var e=n.children;return(0,o.jsx)(u,{children:e})}},695:function(n,e,r){r.d(e,{j:function(){return t.j}});var t=r(777)},922:function(n,e,r){r.r(e);var t=r(695),c=r(184);e.default=function(){return(0,c.jsx)(t.j,{children:"404 Not Found"})}}}]);
-//# sourceMappingURL=922.0466ca65.chunk.js.map
---
+++ static/js/922.0f81aab0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{777:function(n,e,r){r.d(e,{j:function(){return a}});var t,c=r(168),u=r(934).Z.h2(t||(t=(0,c.Z)(["\n  color: red;\n  text-align: center;\n"]))),o=r(184),a=function(n){var e=n.children;return(0,o.jsx)(u,{children:e})}},657:function(n,e,r){r.d(e,{j:function(){return t.j}});var t=r(777)},922:function(n,e,r){r.r(e);var t=r(657),c=r(184);e.default=function(){return(0,c.jsx)(t.j,{children:"404 Not Found"})}}}]);
+//# sourceMappingURL=922.0f81aab0.chunk.js.map
